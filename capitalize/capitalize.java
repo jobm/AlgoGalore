10da440024e42895e7a231ba828858708d30fa0a
JobM <jobmwaniki18@gmail.com>
2015-09-21 11:20:29 +0300

    added capitalize method

--- capitalize/capitalize.java
+++ capitalize/capitalize.java
@@ -3,6 +3,9 @@ public class MainClass{
     public static void GetUserInput(String input){
 
     }
+    public static string Capitalize(String input){
+
+    }
     public static void main(String [] args){
 
     }
