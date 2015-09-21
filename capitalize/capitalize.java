ece1d9300d0e9564e0b383d04b65e2fd461e7aff
JobM <jobmwaniki18@gmail.com>
2015-09-21 11:21:03 +0300

    corrected return type

--- capitalize/capitalize.java
+++ capitalize/capitalize.java
@@ -3,7 +3,7 @@ public class MainClass{
     public static void GetUserInput(String input){
 
     }
-    public static string Capitalize(String input){
+    public static String Capitalize(String input){
 
     }
     public static void main(String [] args){
