5a9508a450e45ab300797071cf01b2b55d04035a
JobM <jobmwaniki18@gmail.com>
2015-09-21 11:38:54 +0300

    added the split loop

--- capitalize/capitalize.java
+++ capitalize/capitalize.java
@@ -3,7 +3,12 @@ public class MainClass{
     public static void GetUserInput(String input){
 
     }
-    public static String Capitalize(String input){
+    public static String[] Capitalize(String input){
+        String [] _split = input.split(" ");
+        String [] _capitalized = new String[]{};
+        for(int i = 0; i < _split.length; i++){
+
+        }
 
     }
     public static void main(String [] args){
