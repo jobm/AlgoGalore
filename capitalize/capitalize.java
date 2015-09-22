047c3b541498fcfca4fe9c746989eba412074f84
JobM <jobmwaniki18@gmail.com>
2015-09-22 09:06:04 +0300

    capitalize working

--- capitalize/capitalize.java
+++ capitalize/capitalize.java
@@ -1,17 +1,23 @@
 import java.util.*;
-public class MainClass{
-    public static void GetUserInput(String input){
-
+public class Main{
+    public static void GetUserInput(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string of words");
+        String s = input.nextLine();
+        System.out.println(Capitalize(s.toLowerCase()));
     }
-    public static String[] Capitalize(String input){
-        String [] _split = input.split(" ");
-        String [] _capitalized = new String[]{};
-        for(int i = 0; i < _split.length; i++){
-
+    public static String Capitalize(String input){
+        String [] caps = input.split(" ");
+        String space = " ";
+        String capitalize = "";
+        String formed = " ";
+        for(int i = 0;i < caps.length; i++){
+            capitalize = caps[i].substring(0,1).toUpperCase()+caps[i].substring(1,caps[i].length())+space;
+            formed += capitalize;
         }
-
+        return formed;
     }
     public static void main(String [] args){
-
+        GetUserInput();
     }
 }
