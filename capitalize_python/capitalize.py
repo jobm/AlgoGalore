d43263f389c8016bb1cbd0d8f5e41e334d587d58
JobM <jobmwaniki18@gmail.com>
2015-09-30 10:02:46 +0300

    added python capitalize

--- capitalize_python/capitalize.py
+++ capitalize_python/capitalize.py
@@ -2,3 +2,16 @@
 # the first letter of each word. Words will be separated by only one space.
 # it should take string input from a user
 # do not use the capitalize method
+def get_user_input():
+    str_input = raw_input("Enter a sentence: ")
+    while str_input == "":
+        str_input = raw_input("Enter a sentence: ")
+    else:
+        print(capitalizer(str_input))
+def capitalizer(str_input):
+    list_words = str_input.split( )
+    list_split = []
+    for word in list_words:
+       list_split.append(word.replace(word[0],word[0].upper()))
+    return ' '.join(list_split)
+get_user_input()
