65f442a9a8c9d48be949c1db0a4c5dd180551ea0
JobM <jobmwaniki18@gmail.com>
2015-10-12 18:28:18 +0300

    added cesars cipher

--- ceaser/ceaser_cipher.py
+++ ceaser/ceaser_cipher.py
@@ -7,3 +7,32 @@
 
 # to read more about ceaser's cipher visit http://practicalcryptography.com/ciphers/caesar-cipher/ or google some more
 # happy coding :-)
+
+import string
+def capitalizer(str_input):
+    list_words = str_input.split( )
+    list_split = []
+    for word in list_words:
+       list_split.append(word.replace(word[0],word[0].upper()))
+    return ' '.join(list_split)
+
+
+def cipher(sentence,step):
+    cipher = []
+    alphabets = string.ascii_lowercase
+    alphabets_tuples = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+    alpha_cipher = ''
+    spaces = [pos for pos, char in enumerate(sentence) if char == ' ']
+    for char in sentence.lower(): 
+        if char in alphabets:
+            alpha_cipher = alphabets.index(char) + step
+            if alphabets.index(char) + step >= 25:
+                alpha_cipher = alpha_cipher % 25
+        cipher.append(alphabets_tuples[alpha_cipher])
+    for i in spaces:
+        cipher[i] = ' '
+    return capitalizer(''.join(cipher))
+
+print cipher("Caesar Cipher",2)
+
+
